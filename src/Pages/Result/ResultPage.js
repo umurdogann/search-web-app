7ac47292410fcb508b83ee63d48d305cabd2f616
Cuma Umur Doğan <umurdogann@gmail.com>
2021-12-30 03:09:59 +0300

    Add functionality to ResultPage

--- src/Pages/Result/ResultPage.js
+++ src/Pages/Result/ResultPage.js
@@ -1,25 +1,43 @@
-import React from 'react';
-import { Container, Left, Logo, OrderByContainer, ResultWrapper, Right} from './styled';
+import React, { useEffect, useState } from 'react';
+import { Container, Left, Logo, OrderByContainer, ResultWrapper, Right } from './styled';
 import logo from '../../Images/logo.svg';
 import SearchBar from '../../Components/SearchBar/SearchBar';
 import SearchResult from '../../Components/SearchResult/SearchResult';
 import PageNumbers from '../../Components/PageNumbers/PageNumbers';
 import OrderButton from '../../Components/OrderButton/OrderButton';
-const ResultPage = () => {
+import { useParams } from 'react-router-dom';
+import { SearchInData, SortData } from '../../Services/SearchService';
+const ResultPage = (props) => {
+	const { searchKey } = useParams();
+	const [data, setData] = useState([]);
+	const [page, setPage] = useState(1);
+
+	useEffect(() => {
+		setData(SearchInData(searchKey));
+		setPage(1);
+	}, [searchKey]);
+
+	const handleOrderTypeChange = (type) => {
+		setData(SortData(data, type));
+	};
+
 	return (
 		<Container>
 			<Left>
 				<Logo src={logo} />
 			</Left>
 			<Right>
-				<SearchBar />
+				<SearchBar searchKey={searchKey} />
 				<ResultWrapper>
-				<OrderByContainer>
-					<OrderButton/>
-				</OrderByContainer>
-				<SearchResult forPage={true} />
+					<OrderByContainer>
+						<OrderButton orderCallback={handleOrderTypeChange} />
+					</OrderByContainer>
+					<SearchResult
+						data={data.slice(page === 1 ? 0 : (page - 1) * 6, (page - 1) * 6 + 6)}
+						forPage={true}
+					/>
 				</ResultWrapper>
-                <PageNumbers/>				
+				<PageNumbers totalPage={Math.ceil(data.length / 6)} page={page} callback={setPage} />
 			</Right>
 		</Container>
 	);
--- src/Pages/Result/styled.js
+++ src/Pages/Result/styled.js
@@ -40,6 +40,7 @@ export const OrderByContainer = styled.div`
 
 export const ResultWrapper = styled.div`
 	width: 100%;
+	min-height: 60vh;
 	display: flex;
 	flex-direction: column;
 	gap: 10px;
