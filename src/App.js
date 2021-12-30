dc1f656eaa13d7fd2f5f109923f1069655f00aa5
Cuma Umur Doğan <umurdogann@gmail.com>
2021-12-30 03:07:13 +0300

    Add routing functionality to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react'
-
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LandingPage from './Pages/Landing/LandingPage';
+import ResultPage from './Pages/Result/ResultPage';
+import mockData from './mockData.json';
 const App = () => {
-  return (
-    <div>
-      Search-Web-App
-    </div>
-  )
-}
+	localStorage.setItem('mockData', JSON.stringify(mockData));
 
-export default App
+	return (
+		<>
+			<BrowserRouter>
+				<Routes>
+					<Route exact path='/' element={<LandingPage />} />
+					<Route exact path='/result/:searchKey' element={<ResultPage />} />
+				</Routes>
+			</BrowserRouter>
+		</>
+	);
+};
+
+export default App;
