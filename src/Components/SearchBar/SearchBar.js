43e2fc006e409e94b6682255665c31f1b9f67e88
Cuma Umur Doğan <umurdogann@gmail.com>
2021-12-30 03:02:46 +0300

    Add functionality to search bar component for searching something on the string

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Container, SearchInput, SearchButton } from './styled';
-const SearchBar = () => {
+const SearchBar = (props) => {
+	const { callback, searchKey } = props;
+	const [text, setText] = useState(searchKey ? searchKey : '');
+	const navigate = useNavigate();
+	const handleOnChange = (e) => {
+		callback && callback(e.target.value);
+		setText(e.target.value);
+	};
+	const handleClick = () => {
+		navigate('/result/' + text);
+	};
 	return (
 		<Container>
-			<SearchInput placeholder='Search Something...' type='text' />
-			<SearchButton>Search</SearchButton>
+			<SearchInput placeholder='Search Something...' type='text' value={text} onChange={handleOnChange} />
+			<SearchButton onClick={handleClick}>Search</SearchButton>
 		</Container>
 	);
 };
